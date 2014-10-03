ce5838d80bf2013e4903e666b8e0f1a6dabdd27a
Mason Fabel <mason.fabel@gmail.com>
2014-10-03 12:29:32 -0700

    Formatting and commenting

--- main.c
+++ main.c
@@ -6,82 +6,91 @@
 #include <sys/wait.h>
 #include "list.h"
 #include "makearg.h"
+
+/* msh main loop */
 int main(int argc, char** argv) {
-  /* variables */
-  char buff;
+	char buff;
 	char* str;
-  char** argVals;
-  int argNum;
-  int i;
-  int running;
-  int status;
-  pid_t pid;
-  struct node* head;
+	char** argVals;
+	int argNum;
+	int i;
+	int running;
+	int status;
+	pid_t pid;
+	struct node* head;
 
-  /* process arguments */
+	argNum = 0;
+	running = 1;
+	head = NULL;
+	argVals = NULL;
 
-  /* initialize */
-  argNum = 0;
-  running = 1;
-  head = NULL;
-  argVals = NULL;
+	/* main loop */
+	while(running) {
 
-  /* main loop */
-  while(running) {
+		/* print prompt */
+		fprintf(stdout, "?: ");
 
-    /* get input */
-    fprintf(stdout, "?: ");
-    do {
-      buff = (char) getchar();
-      listAdd(&head, buff);
-    } while(buff != '\n');
+		/* add each character up to and including the '\n' into a linked list
+		   of characters */
+		do {
+			buff = (char) getchar();
+			listAdd(&head, buff);
+		} while(buff != '\n');
 
-    
-    /* parse input */
+		/* convert the linked list of characters into a string */
+		str = listToString(head);
+		
+		/* convert the string into an array of strings, one string for each word */
+		argNum = makearg(str, &argVals);
+		if(argNum == -1) {
+			fprintf(stderr, "msh: unable to parse input\n");
+			break;
+		}
 
+		/* free intermediate list */
+		listFree(&head);
+		head = NULL;
 
-	str = listToString(head);
-    
-    argNum = makearg(str, &argVals);
+		/* free intermediate string */
+		free(str);
+		str = NULL;
 
-	free(str);
-	str = NULL;
-    listFree(&head);
-    head = NULL;
+		/* check for the "exit" shell builtin */
+		if(!strcmp(argVals[0], "exit")) {
+			running = 0;
+			break;
+		}
 
-    /* builtins */
-    if(!strcmp(argVals[0], "exit")) {
-      running = 0;
-      break;
-    }
+		/* fork a new process */
+		pid = fork();
 
-    /* fork */
-    pid = fork();
-    if(pid == -1) {
+		/* check the return value of fork() */
+		if(pid == -1) {
+			/* error - abort */
+			fprintf(stderr, "msh: unable to fork.\n");
+			break;
 
-      /* error */
-      fprintf(stderr, "msh: unable to fork.\n");
-      break;
-    } else if(pid == 0) {
+		} else if(pid == 0) {
+			/* child - exec command */
+			if(execvp(argVals[0], argVals) == -1) {
+				fprintf(stderr, "msh: unable to execute %s\n", argVals[0]);
+				exit(1);
+			}
 
-      /* child -- exec command */
-      if(execvp(argVals[0], argVals) == -1) {
-	fprintf(stderr, "msh: unable to execute %s\n", argVals[0]);
-	exit(1);
-      }
-      exit(0);
-    } else if(pid > 0) {
+			exit(0);
 
-      /* parent -- wait for child */
-      wait(&status);
-    }
-  for(i = 0; i < argNum; i++) {
-    free(argVals[i]);
-  }
-  free(argVals);
-    
-  }
+		} else if(pid > 0) {
+			/* parent - wait for child */
+			wait(&status);
+		}
 
-  /* Clean Up */ 
-  exit(0);
+		/* clean up data structures */
+		for(i = 0; i < argNum; i++) {
+			free(argVals[i]);
+		}
+		free(argVals);
+		
+	}
+
+	exit(0);
 }
--- makearg.c
+++ makearg.c
@@ -2,18 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 #include "makearg.h"
+
+/* check if a character is whitespace */
 int isWhitespace(char ch) {
 	int res;
+
+	/* ' ', '\t', '\n', and '\0' are all considered whitespace, as calling
+	   '\0' whitespace makes the argc loop in makearg() cleaner */
 	switch(ch) {
-	case ' ':
-	case '\t':
-	case '\n':
-	case '\0':
-		res = 1;
-		break;
-	default:
-		res = 0;
+		case ' ':
+		case '\t':
+		case '\n':
+		case '\0':
+			res = 1;
+			break;
+		default:
+			res = 0;
 	}
+	
 	return res;
 }
 
@@ -23,48 +29,59 @@ int makearg(char* s, char*** args) {
 	char* ch;
 	char* end;
 
-	/* count words */
-
 	argc = 0;
 
+	/* count words, where a "word" is a sequence of characters of nonzero
+	   length followed by whitespace */
 	for(ch = s; ch < (strlen(s) + s); ch++) {
+		/* add one to argc when whitespace is found */
 		argc += isWhitespace(*ch);
+
+		/* skip th rest of the whitespace */
 		for(; isWhitespace(*ch); ch++) {
 			/* Do Nothing */
 		}
 	}
 
-	/* parse words */
-
-	*args = (char**) malloc((argc * (sizeof(char*))+1)); /*CHANGE HERE @ +1 */
-
+	/* allocate an array of strings one larger than the number of arguments,
+	   so that there is room for the final NULL */
+	*args = (char**) malloc((argc * (sizeof(char*)) + 1));
 	if(*args == NULL) goto fail;
 
 	ch = s;
 	end = s;
 
+	/* copy each argument from s into args */
 	for(i = 0; i < argc; i++) {
+		/* move end to the end of the current word */
 		for(end = ch; !isWhitespace(*end); end++) {
 			/* Do Nothing */
 		}
+
+		/* mark the end of the word with '\0' */
 		*end = '\0';
 
+		/* allocate a new string inside of args long enough for the string and a
+		   final '\0' */
 		*((*args) + i) = malloc((strlen(ch) + 1) * sizeof(char));
 		if(*((*args) + i) == NULL) goto fail;
 
+		/* copy the string from s to args */
 		strcpy(*((*args) + i), ch);
 
-		for(ch = end; isWhitespace(*ch); ch++) { /*CHANGED to END from END+1 */
+		/* move the start of the word to the first non-whitespace character past
+		   end */
+		for(ch = end; isWhitespace(*ch); ch++) {
 			/* Do Nothing */
 		}
 	}
 
+	/* NULL terminate args */
 	(*((*args) + (argc))) = NULL; 
-	
-	ch = '\0';
 
 	return argc;
- fail:
-	fprintf(stderr, "FAIL\n");
-	return -1;
+
+	/* return an error code */
+	fail:
+		return -1;
 }
